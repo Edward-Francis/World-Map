3d789190f58f0734ff1c53982c70c2565b25c557
Edward Francis <edward.francis@zpg.co.uk>
2019-07-16 10:15:27 +0100

    Added Google Map to page

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,3 +1,31 @@
 <template>
-    <h1>Map page</h1>
+  <div class="Map" />
 </template>
+
+<script>
+import gmapsInit from "@/utils/gmaps";
+export default {
+  name: "Map",
+  async mounted() {
+    try {
+      const google = await gmapsInit();
+      const geocoder = new google.maps.Geocoder();
+      const map = new google.maps.Map(this.$el);
+
+      geocoder.geocode({ address: "Europe" }, results => {
+        map.setCenter(results[0].geometry.location);
+        map.fitBounds(results[0].geometry.viewport);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+</script>
+
+<style>
+.Map {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
