ca9da2838b5fcea7ad8862b0ed925ebfd5b56a38
Edward Francis <edward.francis@zpg.co.uk>
2019-07-16 09:28:18 +0100

    New view for map display

--- src/router.js
+++ src/router.js
@@ -21,6 +21,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/map",
+      name: "map",
+      component: () => import(/* webpackChunkName: "about" */ "./views/Map.vue")
     }
   ]
 });
